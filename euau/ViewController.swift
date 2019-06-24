c759b83218f3cfc27743d1c400918d3a59877edf
student <student@student21.local>
2019-06-24 09:20:44 -0300

    Initial Commit

---
+++ euau/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  euau
+//
+//  Created by student on 24/06/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
