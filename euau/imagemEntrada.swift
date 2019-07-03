e5869c7d55e724b69dab1a7b9253cd42a6314634
student <student@student24.local>
2019-07-03 08:51:01 -0300

    commit lunchScreen

---
+++ euau/imagemEntrada.swift
@@ -0,0 +1,23 @@
+//
+//  imagemEntrada.swift
+//  euau
+//
+//  Created by student on 02/07/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import Foundation
+
+class imagem {
+    
+    let foto: String
+    
+    init(foto: String){
+        self.foto = foto
+    }
+    
+}
+
+class imagemDao{
+    
+}
